cbc0236dc7a80ae6951a8226ee4372acf320a9b2
Hangjit Rai <rai.hangjit@gmail.com>
2019-09-10 15:27:27 +1000

    Completed Sec 6

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,11 @@ class Quiz extends Component {
     this.state={quiz_position:1}
   }
 
+  showNextQuestion(){
+    this.setState((state)=>{
+      return {quiz_position:state.quiz_position+1};
+    });
+  }
   render() {
     const isQuizEnd =(this.state.quiz_position-1) === quizData.quiz_questions.length;
 
@@ -18,7 +23,7 @@ class Quiz extends Component {
       <div>
         {isQuizEnd?
           <QuizEnd />:
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} />
         }
       </div>
     );
